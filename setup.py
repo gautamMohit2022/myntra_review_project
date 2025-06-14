5a709051e05129c5b9ae34f233c304e6ad3c2556
Mohit Gautam <gautampm2006@gmail.com>
2025-06-14 08:47:53 +0530

    This is my Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+setup(
+    name='scrapper',
+    version='0.0.1',
+    author='gautam',
+    author_email='gautampm2006@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+
+
+)
